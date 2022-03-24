3531a3d28e1516dd1de2cf8d96bce7450357a0bd
Carururu <cdvsalanga@gmail.com>
2022-03-24 15:53:28 +0800

    React routing

---
+++ src/pages/LoginPages.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginPages = () => {
+  return (
+    <div>
+      LoginPages
+      <Link to='/'>Home</Link>
+    </div>
+  )
+}
+
+export default LoginPages
